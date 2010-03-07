6c97e7d05255ba3b8f3ae31648c016ccc27f537a
Yuta Namiki <namiki@vmware-debian-etch.(none)>
2010-03-08 00:17:06 +0900

    add files.

---
+++ app/helpers/menu_helper.rb
@@ -0,0 +1,20 @@
+module MenuHelper
+  include Testrepos::I18n
+
+  MENUS = [{:name => "specification", :url => {:controller => 'testprojects', :action => "index"}},
+           {:name => "testplan",      :url => {:controller => 'testprojects', :action => "index"}},
+           {:name => "result",        :url => {:controller => 'testprojects', :action => "index"}},
+          ]
+  def render_main_menu(selected)
+    links = []
+    MENUS.each do |item|
+      label = item[:label] || 'label_' + item[:name]
+      config = {}
+      if item[:name] == selected
+        config[:class] = 'selected'
+      end
+      links << content_tag('li', link_to(l(label), item[:url], config))
+    end
+    links.empty? ? nil : content_tag('ul', links.join("\n"))
+  end
+end
